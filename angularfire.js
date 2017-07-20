be28e2f9d143576df756f0f304898cd97a823a94
emilyb22 <emilyb22@live.ca>
2017-07-20 15:37:51 -0400

    making firebase work

---
+++ angularfire.js
@@ -0,0 +1,45 @@
+angular
+	.module('hockeyApp', ['firebase'])
+    .constant('firebaseConfig', {
+        apiKey: "AIzaSyDma2d0IC9vGyhu8zTNBHo0M6sMx-Zrs0k",
+    authDomain: "hockey-app-f0b7c.firebaseapp.com",
+    databaseURL: "https://hockey-app-f0b7c.firebaseio.com",
+    projectId: "hockey-app-f0b7c",
+    storageBucket: "",
+    messagingSenderId: "1017524189091"
+})
+.run(firebaseConfig => firebase.initializeApp(firebaseConfig))
+.controller('hockeyCtrl', function($scope, $firebaseObject, $firebaseArray){
+	const dbRef = firebase.database().ref().child('players')
+    
+    $scope.playersList = $firebaseArray(dbRef)
+    
+    $scope.player = $firebaseObject(dbRef.child('-KpWOmUzEbd1aaaedlpj'))
+    
+    this.getBlankPlayer = () => ({
+        name:'',
+        team: '',
+        draft: '',
+        birthday: '',
+        birthplace: '',
+    })
+    
+    $scope.newPlayer = this.getBlankPlayer()
+    
+    $scope.addPlayer = () => {
+        $scope.playersList.$add($scope.newPlayer)
+        $scope.newPlayer = this.getBlankPlayer()
+    }
+    
+    $scope.savePlayer = player => $scope.playersList.$save(player)
+    
+    $scope.removePlayer = player => {
+        if(confirm("Are you sure you want to delete this player?")){
+    $scope.playersList.$remove(player)
+        }
+    }
+    
+    
+})
+
+
